379838ff79f171e39715855433c62e68d18fa755
Lara Ewellen <Laraewellen@gmail.com>
2025-02-20 09:54:05 -0300

    update

--- exemploInicial.py
+++ exemploInicial.py
@@ -1,8 +1,8 @@
 n = 30
 numDivisores = 0
 
-for i in ranger (1, n +1)
-   if n % == 0:
-    numDivisores = numDivisores +1
+for i in range(1, n + 1):
+    if n % i == 0:  
+        numDivisores += 1  
 
 print(numDivisores)
