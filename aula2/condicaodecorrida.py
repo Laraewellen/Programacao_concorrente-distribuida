8484254acdd6be20773a2c9b27847ace0d4f3c22
Lara Ewellen <laraewellen@gmail.com>
2025-02-27 10:54:22 -0300

    condição de corrida varias threads acessando a mesma informação

--- aula2/condicaodecorrida.py
+++ aula2/condicaodecorrida.py
@@ -7,9 +7,9 @@ Contador = 0
 
 def incrementar():
     global Contador
-    for _ in range(5000):
+    for _ in range(5000): #reduzido o n de irerações por thread
         V = Contador
-        time.sleep(0.001)
+        time.sleep(0.001) #atraso artificial para aumentar a chance de colisões
         Contador = V + 1
 threads = []
 
